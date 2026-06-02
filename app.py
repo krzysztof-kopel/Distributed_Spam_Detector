9dbeaaba07f0bbd1adedf4cdb6e0ac257b456b1a
KK04 <72666836+krzysztof-kopel@users.noreply.github.com>
2026-06-02 19:44:47 +0200

    Vocabulary loading improved.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 os.environ["TF_USE_LEGACY_KERAS"] = "1" # Only legacy Keras works well with Ray
 
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 import ray
 import tensorflow as tf
 
@@ -18,7 +18,11 @@ class SpamClassifierService:
         self.model = tf.keras.models.load_model(os.path.abspath("artifacts/model.keras"))
         self.vectorizer = tf.keras.layers.TextVectorization(max_tokens=10000, output_sequence_length=200)
         with open(os.path.abspath("artifacts/vocabulary.txt"), encoding="utf-8") as file:
-            vocabulary = [i.strip() for i in file.readlines() if i.strip()]
+            raw_vocabulary = file.read().splitlines()
+        vocabulary = [
+            token.strip() for token in raw_vocabulary
+            if token.strip() and token.strip() not in ("", "[UNK]")
+        ]
         self.vectorizer.set_vocabulary(vocabulary)
 
     def predict_spam(self, payload: EmailService):
@@ -26,7 +30,7 @@ class SpamClassifierService:
         text_vectorized = self.vectorizer([raw_text]).numpy()
         result = self.model.predict(text_vectorized)[0][0]
         result = float(result)
-        return {"probability": result, "result": "Spam" if result > 0.5 else "Normal email"}
+        return {"Spam probability": result, "result": "Spam" if result > 0.5 else "Normal email"}
 
 
 @ray.serve.deployment
